cf92b815c37090280ec0fafb78257f099c6d5d4b
sonng17 <ndson1703@gmail.com>
2025-01-11 08:59:04 +0700

    product env

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -34,6 +34,7 @@ export default function SignIn() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
+        credentials: "include",
       });
       const data = await res.json();
       if (data.success === false) {
